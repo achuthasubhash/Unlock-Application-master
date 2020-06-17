c15b18cfcfdc7537dc57850716e398e52bb63a25
achuthasubhash <36812519+achuthasubhash@users.noreply.github.com>
2020-06-17 14:46:43 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 14:21:08 2020
+
+@author: DELL
+"""
+
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+# Get the training data we previously made
+data_path = './Images/'
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+
+# Create arrays for training data and labels
+Training_Data, Labels = [], []
+
+# Open training images in our datapath
+# Create a numpy array for training data
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+
+# Create a numpy array for both training data and labels
+Labels = np.asarray(Labels, dtype=np.int32)
+
+#LBPHFaceRecognizer  train model
+# Initialize facial recognizer
+model = cv2.face.LBPHFaceRecognizer_create()
+
+# NOTE: For OpenCV 3.0 use cv2.face.createLBPHFaceRecognizer()
+
+# Let's train our model 
+model.train(np.asarray(Training_Data), np.asarray(Labels))
+print("Model trained sucessefully")
+
+model.save("model.h5")
